7dd762a5ce79b88b0fc5a45454312e0110e641f8
ztzzhi <9972792+zhangtingzhi@user.noreply.gitee.com>
2023-04-06 17:23:11 +0800

    update

--- src/config/config.default.js
+++ src/config/config.default.js
@@ -14,3 +14,19 @@ let config = {
 }
 
 module.exports = config
+// let config = {
+//     APP_PORT: 7001,
+//     MYSQL_HOST: 'localhost',
+//     MYSQL_PORT: 3306,
+//     MYSQL_USER: 'nuxtapp',
+//     MYSQL_PWD: 'Mp8BZmFP76xJx8MT',
+//     MYSQL_DB: 'nuxtapp',
+//     MYSQL_TYPE: 'mysql',
+//     SALT: 10,//密码加密 加盐 10 次,
+//     JWTSECRET: 'ztz',
+//     APPID:'wx6108bf2c6ba79156',
+//     SECRET:'13a41762fa0d21dec0aaeb729040352a',
+//     APIKEY:'078b65ada7136beb6a432a3499b71f43',
+// }
+
+// module.exports = config
