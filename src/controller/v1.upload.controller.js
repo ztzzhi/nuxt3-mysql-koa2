b0fd87d1d121b8f5a43e49f5589e4971aba1a288
ztzzhi <9972792+zhangtingzhi@user.noreply.gitee.com>
2023-04-07 15:14:42 +0800

    update:修复dev文件丢失的问题

--- src/controller/v1.upload.controller.js
+++ src/controller/v1.upload.controller.js
@@ -23,59 +23,6 @@ class UserController {
       result: `http://180.76.121.2:7001/${newFilename}` //换成本地（http://localhost:7001）或者线上你的ip:7001的地址 这是我的服务器地址 大家最好不要上传到我的服务器～
     };
   }
-
-  async getcalc (ctx, next) {
-    try {
-      let result = []
-      let file1 = fs.readdirSync(path.join(__dirname, '../static/excle/1')) //实际值
-      let file2 = fs.readdirSync(path.join(__dirname, '../static/excle/2')) //理论值
-      let obj1 = xlsx.parse(path.join(__dirname, '../static/excle/1/' + file1[0]))
-      let obj2 = xlsx.parse(path.join(__dirname, '../static/excle/2/' + file2[0]))
-      let actualValueArr = obj1[0]["data"].slice(1).flat(Infinity)
-      let expectedValueArr = obj2[0]["data"].slice(1).map(item => {
-        return item.filter(it => typeof it === "number")
-      })
-
-      actualValueArr.forEach(out => {
-        expectedValueArr.forEach(inner => {
-          if (Math.abs(out - inner[0]) / inner[1] * 1000000 < 20) {
-            result.push([out])
-          }
-        })
-      })
-
-
-      let xlsxObj = [
-        {
-          name: 'Sheet1',
-          data: [
-            ["m/z"],
-            ...result
-          ],
-        }]
-      var newFilename = new Date().getTime()
-      fs.writeFileSync(path.join(__dirname, `../static/excle/${newFilename}.xlsx`), xlsx.build(xlsxObj), "binary")
-
-      return ctx.body = {
-        code: 200,
-        msg: '解析成功',
-        result: [...new Set(result)],
-        length: [...new Set(result)].length,
-        url: `http://180.76.121.2:7001/excle/${newFilename}.xlsx`
-      };
-    } catch (error) {
-      console.log(error);
-      return ctx.app.emit(
-        "error",
-        {
-          code: 500,
-          msg: "服务器异常",
-          result: "",
-        },
-        ctx
-      );
-    }
-  }
 }
 
 module.exports = new UserController();
